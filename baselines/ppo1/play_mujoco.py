e7dd4cfd806cc04330cc2d81339e686cf9c49904
syllogismos <anilkaraka@live.in>
2018-07-03 18:25:17 +0530

    video recorder stuff

--- baselines/ppo1/play_mujoco.py
+++ baselines/ppo1/play_mujoco.py
@@ -10,21 +10,34 @@ def main():
     args = mujoco_arg_parser().parse_args()
     logger.configure()
     pi = train(args.env, num_timesteps=1, seed=args.seed, play=False)
-    model_path = '/home/ubuntu/wandb_baselines/wandb/run-20180702_220411-4xtopfue/humanoid_policy'
+    run_home = '/home/ubuntu/wandb_baselines/wandb/run-20180702_220411-4xtopfue/'
+    model_path = run_home + 'humanoid_policy'
+    # model_path = '/home/ubuntu/wandb_baselines/wandb/run-20180702_220411-4xtopfue/humanoid_policy'
     U.load_state(model_path)
     env = make_mujoco_env('RoboschoolHumanoid-v1', seed=0)
     tot_r = 0
     ob = env.reset()
     runs = 0
+    video = False
+    if video:
+        video_recorder = gym.wrappers.monitoring.video_recorder(env=env, base_path=os.path.join(run_home, 'humanoid_run_%i'%runs), enabled=True)
+
     while True:
         action = pi.act(stochastic=False, ob=ob)[0]
         ob, r, done, _ = env.step(action)
+        if video:
+            video_recorder.capture_frame()
         tot_r += r
         if done:
+            
+            ob = env.reset()  
             runs += 1
+            if video:
+                video_recorder.close()
+                video_recorder = gym.wrappers.monitoring.video_recorder(env=env, base_path=os.path.join(run_home, 'humanoid_run_%i'%runs), enabled=True)
+                
             print(tot_r)
             print("@@@@@@@@@@@@@@@")
-            ob = env.reset()  
         if runs > 3:
             break
 
